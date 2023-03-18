485f4faab97c98679d1ac7348b1b45f162cdce47
weewoo123 <samuelhe2014@gmail.com>
2023-03-18 00:52:06 -0500

    fixed data formatting

--- app/db.py
+++ app/db.py
@@ -1,6 +1,7 @@
 import psycopg2
 import os
-DATABASE_URL = os.getenv('DATABASE_URL')
+# DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = "postgres://tbptunbssokuks:df66dff65cf6af8223a6696a4aa8a0f22aaefbe8ef3dc6accd643a5e64d9abd7@ec2-3-223-213-207.compute-1.amazonaws.com:5432/da38dpv7bfap34"
 ############################ DRY WEIGHT FUNCTIONS ############################
 #INPUTS:
 #solution: string
